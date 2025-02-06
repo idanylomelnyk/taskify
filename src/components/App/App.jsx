ca0569b3168d480719125dc24d022454b3646f98
Danylo <idanylomelnyk@gmail.com>
2025-02-06 11:54:19 +0200

    Save tasks to localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import Navigation from "../Navigation/Navigation";
+import { useEffect, useState } from "react";
 import { Box, Container, CssBaseline } from "@mui/material";
+import Navigation from "../Navigation/Navigation";
 import HomePage from "../../pages/HomePage";
 
 export default function App() {
+  const tasksToState = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  const [tasks, setTasks] = useState(tasksToState);
+
+  useEffect(() => {
+    const stateToStorage = JSON.stringify(tasks);
+    localStorage.setItem("tasks", stateToStorage);
+  }, [tasks]);
+
   return (
     <>
       <CssBaseline />
@@ -11,10 +21,96 @@ export default function App() {
         <Navigation />
         <Container maxWidth='false'>
           <Routes>
-            <Route path='/' element={<HomePage />} />
+            <Route
+              path='/'
+              element={<HomePage tasks={tasks} setTasks={setTasks} />}
+            />
           </Routes>
         </Container>
       </Box>
     </>
   );
 }
+
+// {
+//   id: nanoid(),
+//   title: "My first task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate ipsum a vel sapiente?",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My second task",
+//   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My third task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My forth task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate ipsum.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My fifth task",
+//   description: "Lorem ipsum dolor sit amet consectetur.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My sixth task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate ipsum a vel sapiente?",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My seventh task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut fugit neque atque quia explicabo ex earum alias enim odit vel officia nesciunt?",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My eighth task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My nineth task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My tenth task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My eleventh task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut fugit neque atque quia.",
+//   complete: false,
+// },
+// {
+//   id: nanoid(),
+//   title: "My twelfth task",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut fugit neque atque quia explicabo ex earum alias enim odit vel officia nesciunt?",
+//   complete: false,
+// },
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,96 +1,11 @@
 import { Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
-import { nanoid } from "nanoid";
+
 import TaskList from "../components/TaskList/TaskList";
 import NewTaskForm from "../components/NewTaskForm/NewTaskForm";
 import NoTasksNotice from "../components/NoTasksNotice/NoTasksNotice";
 
-export default function HomePage() {
-  const [tasks, setTasks] = useState([
-    {
-      id: nanoid(),
-      title: "My first task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate ipsum a vel sapiente?",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My second task",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My third task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My forth task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate ipsum.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My fifth task",
-      description: "Lorem ipsum dolor sit amet consectetur.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My sixth task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nobis culpa voluptate ipsum a vel sapiente?",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My seventh task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut fugit neque atque quia explicabo ex earum alias enim odit vel officia nesciunt?",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My eighth task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My nineth task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My tenth task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My eleventh task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut fugit neque atque quia.",
-      complete: false,
-    },
-    {
-      id: nanoid(),
-      title: "My twelfth task",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi, itaque voluptatibus vero animi nam laudantium ut fugit neque atque quia explicabo ex earum alias enim odit vel officia nesciunt?",
-      complete: false,
-    },
-  ]);
-
+export default function HomePage({ tasks, setTasks }) {
   const [openNewTaskModal, setOpenNewTaskModal] = useState(false);
   const handleOpenNewTaskModal = () => setOpenNewTaskModal(true);
   const handleCloseNewTaskModal = () => {
